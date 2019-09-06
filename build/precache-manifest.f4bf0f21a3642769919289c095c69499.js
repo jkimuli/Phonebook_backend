58e3423734e771fd954ce276ddc330e56ec40397
jkimuli <jkimuli@gmail.com>
2019-09-06 13:11:19 +0300

    Adding the React Production build

---
+++ build/precache-manifest.f4bf0f21a3642769919289c095c69499.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099767c2892d798a1d30db487feec62d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10e46d2f67c0838fd1ee",
+    "url": "/static/js/2.5a4c7492.chunk.js"
+  },
+  {
+    "revision": "e1ba0c21ffa2942013ff",
+    "url": "/static/js/main.8cfa6b06.chunk.js"
+  },
+  {
+    "revision": "edd81c791ba6e791f66c",
+    "url": "/static/js/runtime~main.a09c8fc1.js"
+  }
+]);
